d6c4784f8ec1b92820f40885b2d1608632d65c8d
Ilan Cosman <ilancosman@gmail.com>
2020-03-25 22:45:34 -0700

    Add fzf/yay remove installed package abbr

--- abbrs.zsh
+++ abbrs.zsh
@@ -15,8 +15,9 @@ abbr gs "git status"
 
 #-------Package Managers-------
 abbr syu "sudo pacman -Syu"
-abbr spac "pacman -Slq | fzf -m --preview 'pacman -Si {1}' | xargs -ro sudo pacman -Syu"
-abbr syay "yay -Slq | fzf -m --preview 'yay -Si {1}'| xargs -ro yay -Syu"
+abbr spac "pacman -Slq | fzf -m --preview 'pacman -Si {1}' | xargs -ro sudo pacman -Syu" # Search through all available packages and install selected
+abbr syay "yay -Slq | fzf -m --preview 'yay -Si {1}'| xargs -ro yay -Syu" # Same as above but use yay to also search through the AUR
+abbr ryay "yay -Qeq | fzf -m --preview 'yay -Qi {1}' | xargs -ro yay -Rs" # Search through list of installed packags and remove selected
 
 #------------Safety------------
 abbr rm "rm -I ^"
